b0eb5632df25603ef8a76c2bc07fb9ff7edd9ff5
Dave Gaeddert <dave.gaeddert@gmail.com>
2017-06-28 13:27:35 -0500

    fix available versions and remove * from installed version in available

--- collect.php
+++ collect.php
@@ -3,6 +3,11 @@
 $collected = array();
 
 $schema_version = function($version) {
+    // the currently installed version has an * in front
+    if(substr( $version, 0, 2 ) === "* ") {
+        $version = substr( $version, 2 );
+    }
+
     // dependencies.io schema expects it in this form
     return array('version' => $version);
 };
@@ -16,8 +21,7 @@ foreach ($installed['installed'] as $package) {
     $installed_version = $package['version'];
 
     $info_output = shell_exec("composer show $name --all");
-
-    preg_match('/^versions : (.*)$/m', $input, $matches);
+    preg_match('/^versions : (.*)$/m', $info_output, $matches);
 
     if (count($matches) > 1) {
         $versions_string = $matches[1];
@@ -41,5 +45,3 @@ foreach ($installed['installed'] as $package) {
 // send the final output to stdout so dependencies.io can pick it up
 $final_output = json_encode(array('dependencies' => $collected));
 echo('BEGIN_DEPENDENCIES_SCHEMA_OUTPUT>' . $final_output . '<END_DEPENDENCIES_SCHEMA_OUTPUT');
-
-?>
